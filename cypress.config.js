cabd8166ad7ca82e99dc3cf8256e7654d350c369
Sabine <sabine.draeger_contractor@celanese.com>
2026-09-03 17:59:53 -0300

    Publicação no GH Pages

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
   },
 });
